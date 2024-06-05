36a5aba981b16e36a48e459611a7cb110e058229
Strange4 <83730323+Strange4@users.noreply.github.com>
2024-06-05 12:46:12 -0400

    add prototype game state

---
+++ arrows2.py
@@ -0,0 +1,14 @@
+import random
+from enum import Enum
+
+
+class ArrowDirection(Enum):
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
+
+
+def getRandomArrowDirection() -> ArrowDirection:
+    index = random.randint(0, len(ArrowDirection))
+    return ArrowDirection(index)
--- gameLogic.py
+++ gameLogic.py
@@ -1,4 +1,10 @@
+import arrows2
 
 
+class GameState:
 
-    
+    def __init__(self) -> None:
+        self.player1Points = 0
+        self.player2Points = 0
+        self.roundNumber = 0
+        self.currentDirection = arrows2.getRandomArrowDirection()
