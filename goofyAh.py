5bea2af3d56743e74d2a3d3f4dd595c424441152
Klisea Plaku <kliseaplaku@gmail.com>
2024-12-24 03:56:49 -0500

    added player 1 and 2 directly with different controls

--- goofyAh.py
+++ goofyAh.py
@@ -3,52 +3,39 @@ from sys import exit
 from player import Player
 
 def main():
-
-    def playerChoice(playerGroup):
-        print("WE CHOOSING BOYS")
-        playerPrompt = font.render('Please choose players', True, (0, 0, 0))
-        playerPrompt_rect = playerPrompt.get_rect(center=(640,512))
-        screen.blit(playerPrompt, playerPrompt_rect)
-        while True:
-            if event.type == pygame.KEYUP or event.type == pygame.KEYDOWN:
-                print("we in boys")
-                if event.mod & pygame.K_1:
-                    player1 = Player(1)
-                    playerGroup.add(player1)
-                    player1.assignIcon()
-                    print("p1")
-                    return 1
-                if event.mod & pygame.K_2:
-                    player2 = Player(1)
-                    playerGroup.add(player2)
-                    player2.assignIcon()
-                    print("p2")
-                    return 1
-
     pygame.init()
     screen = pygame.display.set_mode((1280, 1024))
     pygame.display.set_caption('Your Mom')
     clock = pygame.time.Clock()
     font = pygame.font.Font('assets/wiiMenuFont.ttf', 100)
-    game_active = False
+    game_active = True
     background = pygame.Surface(screen.get_size())
     background = background.convert()
-    background.fill((250, 250, 250))
+    background.fill((255, 255, 255))
 
     screen.blit(background, (0,0))
 
+    mainMenuBG = pygame.image.load("assets/main_menu_bg.png")
+    screen.blit(mainMenuBG, (0, 0))
+
     playerGroup = pygame.sprite.Group()
+
+    player1 = Player(1)
+    playerGroup.add(player1)
+    player2 = Player(2)
+    playerGroup.add(player2)
+
     pygame.event.clear()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-            if game_active == False:
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                    game_active = True
-                    print("game no longer false")
-                    playerChoice(playerGroup)
+
+        if game_active:
+            playerGroup.update()
+            playerGroup.draw(screen)
+
         pygame.display.update()
         clock.tick(60)
 
