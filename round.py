b03fcc7504d2320b0c1eee511fd4a55659c9ce09
Klisea Plaku <kliseaplaku@gmail.com>
2024-12-26 18:38:16 -0500

    Update round.py

--- round.py
+++ round.py
@@ -1,17 +1,39 @@
+import pygame
+
+
 class Round:
-    def __init__(self, players):
-        self.players = players
+    def __init__(self, round_id, player_1, player_2, screen ):
+        self.round_id = round_id
+        self.player_1 = player_1
+        self.player_2 = player_2
+        self.screen = screen
+        self.players = pygame.sprite.Group(player_1, player_2)
+        self.player_1_direction = None
+        self.player_2_direction = None
         self.is_match = False
+        self.is_current_round = False
+
+
+    def update(self):
+        self.players.draw(self.screen)
+        if self.is_current_round:
+            self.player_1.update()
+            self.player_2.update()
+        else:
+            self.player_1.update_end(self.player_1_direction)
+            self.player_2.update_end(self.player_2_direction)
 
     def check_match(self):
-        if all(player.directionSelected for player in self.players):
-            rotations = [player.selectedRotation for player in self.players]
-            print(f"Rotations: {rotations}")
+        if not self.is_match:
+            self.player_1_direction  = self.player_1.get_input() if self.player_1.get_input() is not None else self.player_1_direction
+            self.player_2_direction = self.player_2.get_input() if self.player_2.get_input() is not None else self.player_2_direction
+            if self.player_1_direction  is not None and self.player_2_direction is not None:
+                if self.player_1_direction == self.player_2_direction:
+                    self.is_match = True
+                self.is_current_round = False
 
-            if len(set(rotations)) == 1:
-                self.is_match = True
-                return True
+    def set_current_round(self, val):
+        self.is_current_round = val
 
-            return False
-
-        return None
+    def get_is_match(self):
+        return self.is_match
