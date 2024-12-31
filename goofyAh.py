e6516b1764704b838ace6d98d191c48c70c5642f
nMWGecko <joshvilda@gmail.com>
2024-12-30 21:31:11 -0500

    First Relay implmentation

--- goofyAh.py
+++ goofyAh.py
@@ -10,11 +10,17 @@ from round import Round
 from score import guessingScore
 from banner import Banner
 from gpiozero import Button
+from gpioEventHandlers import drink
 import random
+import threading
+
 
 ROUND_COUNT = 3
 NUM_PLAYERS = 2
-
+RELAY_P1_PIN = 2
+RELAY_P2_PIN = 3
+drinkingP1Thread = threading.Thread(target=drink,args=(RELAY_P1_PIN,))
+drinkingP2Thread = threading.Thread(target=drink,args=(RELAY_P2_PIN,))
 screen = pygame.display.set_mode((1280, 1024))
 DISPLAYSURF = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
@@ -38,7 +44,6 @@ def main(currentGuesser, player1Score, player2Score):
     wiiBackground1 = pygame.image.load("assets/Matt/matt1.jpg")
 
     wiiBackground2 = pygame.image.load("assets/Matt/matt2.jpg")
-    print("matt2")
     wiiBackground2 = pygame.transform.scale_by(wiiBackground2, 6)
 
     wiiBackground3 = pygame.image.load("assets/Matt/matt3.jpg")
@@ -160,12 +165,14 @@ def main(currentGuesser, player1Score, player2Score):
                             pygame.draw.rect(screen, (26,183,234), pygame.Rect(0, 195, 1280, 500))
                             win_text_1 = marioFont.render(f'PLAYER 2', True, 'white')
                             win_text_2 = marioFont.render(f'DRINKS!', True, 'white')
+                            drinkingP1Thread.start()
                             screen.blit(win_text_1, (130, 255))
                             screen.blit(win_text_2, (230, 455))
                         else:
                             pygame.draw.rect(screen, (26,183,234), pygame.Rect(0, 195, 1280, 500))
                             win_text_1 = marioFont.render(f'PLAYER 1', True, 'white')
                             win_text_2 = marioFont.render(f'DRINKS!', True, 'white')
+                            drinkingP2Thread.start()
                             screen.blit(win_text_1, (130, 255))
                             screen.blit(win_text_2, (230, 455))
 
--- gpioEventHandlers.py
+++ gpioEventHandlers.py
@@ -1,10 +1,10 @@
 import RPi.GPIO as GPIO
 import pygame
 import sys
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(PIN, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(PIN, GPIO.RISING)  #This means you are catching a rising detection - so your button press
-for event in pygame.event.get():
-     if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE) or GPIO.event_detected(PIN):
-          pygame.quit()
-          sys.exit()
+import time
+
+def drink(seconds,relay):
+     GPIO.setmode(GPIO.BCM)
+     GPIO.output(relay,1)
+     time.sleep(seconds)
+     GPIO.output(relay,0)
